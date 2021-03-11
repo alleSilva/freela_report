eef6347b0dc49f904617c8ebbe73af53ad445763
Alesandro Silva <silva_alesandro@outlook.com>
2021-03-11 08:59:18 -0400

    feat: sum hours per day by freelancer

--- lib/freela_report.ex
+++ lib/freela_report.ex
@@ -1,5 +1,28 @@
 defmodule FreelaReport do
-  def hello do
-    :world
+  #alias FreelaReport.Parser
+
+  #@freelancers =
+   # {Daniele, Mayk, Giuliano, Cleiton, Jakeliny, Diego, Danilo, Rafael, Vinicius, Joseph}
+
+  def build(file_name) do
+    "reports/#{file_name}"
+    |> File.stream!()
+    |> Stream.map(fn line -> parse_line(line) end)
+    |> Enum.reduce(report_acc(), fn [name, hour_per_day,_,_,_],report->
+      Map.put(report, name, report[name] + hour_per_day)
+     end)
+  end
+
+  defp parse_line(line) do
+    line
+    |> String.trim()
+    |> String.split(",")
+    |> List.update_at(1, &String.to_integer/1)
+  end
+
+  def report_acc() do
+    freelancers =
+      ["Daniele", "Mayk", "Giuliano", "Cleiton", "Jakeliny", "Diego", "Danilo", "Rafael", "Vinicius", "Joseph"]
+    Enum.into(freelancers, %{}, &{&1, 0})
   end
 end
