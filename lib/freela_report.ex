7b0c4e87a0f80f424b875ef230a125c5f13b43d4
Alesandro Silva <silva_alesandro@outlook.com>
2021-03-11 09:57:44 -0400

    refactor: separate parser and stream to module and use in FreelaReport module

--- lib/freela_report.ex
+++ lib/freela_report.ex
@@ -1,23 +1,12 @@
 defmodule FreelaReport do
-  #alias FreelaReport.Parser
-
-  #@freelancers =
-   # {Daniele, Mayk, Giuliano, Cleiton, Jakeliny, Diego, Danilo, Rafael, Vinicius, Joseph}
+  alias FreelaReport.Parser
 
   def build(file_name) do
-    "reports/#{file_name}"
-    |> File.stream!()
-    |> Stream.map(fn line -> parse_line(line) end)
+    file_name
+    |> Parser.parse_file()
     |> Enum.reduce(report_acc(), fn [name, hour_per_day,_,_,_],report->
       Map.put(report, name, report[name] + hour_per_day)
      end)
-  end
-
-  defp parse_line(line) do
-    line
-    |> String.trim()
-    |> String.split(",")
-    |> List.update_at(1, &String.to_integer/1)
   end
 
   def report_acc() do
