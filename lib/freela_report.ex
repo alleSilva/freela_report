4e7e86f9fff5f413b9c23c6b951725eef6129be4
Alesandro Silva <silva_alesandro@outlook.com>
2021-03-14 22:31:16 -0400

    feat: build complete report

--- lib/freela_report.ex
+++ lib/freela_report.ex
@@ -4,27 +4,41 @@ defmodule FreelaReport do
   def build(file_name) do
     file_name
     |> Parser.parse_file()
-    |> Enum.reduce(report_acc(), fn line, report->
+    |> Enum.reduce(report_acc(), fn line, report ->
         sum_values(line, report)
      end)
   end
 
-  def sum_values([name, hour_per_day, _, m, _], %{:all => all,:hpm => hpm} = report) do
-  all = Map.put(all, name,all[name]  + hour_per_day)
-  hpm = Map.put(hpm, name,hpm[name][m])
-  report
-  |> Map.put(:all, all)
-  |> Map.put(:hpm, hpm)
+  def sum_values([name, hours, _day, month, year],
+    %{
+      :all_hours => all_hours,
+      :hours_per_month => hours_per_month,
+      :hours_per_year => hours_per_year
+    }) do
+      all_hours = merge_maps(all_hours, %{name => hours})
+      hours_per_month = merge_sub_maps(hours_per_month, %{name => %{month => hours}})
+      hours_per_year = merge_sub_maps(hours_per_year, %{name => %{year => hours}})
+
+      build_map(all_hours, hours_per_month, hours_per_year)
   end
 
-  def report_acc() do
-  months = 
-    [:janeiro, :fevereiro,:março, :abril, :maio, :junho, :julho, :agosto, :setembro, :outubro, :novembro, :dezembro]
-    freelancers =
-      [:Daniele, :Mayk, :Giuliano, :Cleiton, :Jakeliny, :Diego, :Danilo, :Rafael, :Vinicius, :Joseph]
-    all = Enum.into(freelancers, %{}, &{&1, 0})
-    hpm = Enum.into(months, %{}, &{&1, 0})
-    freela_hpm = Enum.into(freelancers, %{}, &{&1, hpm})
-    %{:all => all, :hpm => freela_hpm}
+  defp merge_maps(map1, map2) do
+    Map.merge(map1, map2, fn _key, value1, value2 -> value1 + value2 end)
   end
+
+  defp merge_sub_maps(map1, map2) do
+    Map.merge(map1, map2, fn _key, value1, value2 ->
+      merge_maps(value1, value2) end)
+  end
+
+  defp build_map(all_hours, hours_per_month, hours_per_year) do
+    %{
+      :all_hours => all_hours,
+      :hours_per_month => hours_per_month,
+      :hours_per_year => hours_per_year
+    }
+  end
+
+  defp report_acc(), do: build_map(%{}, %{}, %{})
+
 end
